cddb67fdffadec0c879c760d7fcbe9edf11063df
Eric Feltgen <developer.erichier@gmail.com>
2022-01-15 12:09:11 +0100

    updates & removing unused stuff

--- app/src/main/java/tech/erichier/spotifylauncher/MainActivity.java
+++ app/src/main/java/tech/erichier/spotifylauncher/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
 
         Intent intent = getIntent();
         Uri uri = intent.getData();
